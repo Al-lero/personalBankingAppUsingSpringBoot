69a1c52e458a38abf37ab34340f4f178fd434e2f
Al-lero <aleroutieyione@gmail.com>
2024-11-19 11:23:30 +0100

    first commit

---
+++ src/main/java/com/bankprototype/alerobank/AleroBankApplication.java
@@ -0,0 +1,13 @@
+package com.bankprototype.alerobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleroBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AleroBankApplication.class, args);
+    }
+
+}
